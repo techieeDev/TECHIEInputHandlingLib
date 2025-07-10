18aa310d59f644b116bd0a5cf165dab6693164e4
Elyes <126920308+techieeDev@users.noreply.github.com>
2025-07-10 16:00:37 +0100

    Add files via upload

---
+++ MIS.cs
@@ -0,0 +1,16 @@
+﻿namespace TechieInputHandling
+{
+    // MIS:The main identification system
+    public static class MIS
+    {
+        public static Random rand = new Random();
+        public static string get_random_cycle_id()
+        {
+            string id = "CYC_";
+            id += Convert.ToString(rand.Next(100, 999));
+            id += (char)(rand.Next(97, 122));
+            id += Convert.ToString(rand.Next(100, 999));
+            return id;
+        }
+    }
+}
