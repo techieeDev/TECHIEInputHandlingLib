50a0b6092db6a2bacbc5de9b90ecbaae949cbe13
Elyes <126920308+techieeDev@users.noreply.github.com>
2025-07-10 15:59:42 +0100

    Update DMM.cs

--- DMM.cs
+++ DMM.cs
@@ -5,29 +5,52 @@ namespace TechieInputHandling
     {
         private static double positive_power(double a, double n)
         {
-            float b = 1; while (n > 0){
+            double b = 1; while (n > 0)
+            {
                 b *= a;
                 n = n - 1;
-            }return b;
+            }
+            return b;
         }
         // can be just replaced by 'MathF.Pow(a,n)'
         public static double power(double a, double n)
         {
-            if (n > 0){return positive_power(a, n);}else{return 1 / positive_power(a, n);}
+            if (n > 0) { return positive_power(a, n); } else { return 1 / positive_power(a, n); }
+        }
+
+        public static int clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            else if (value > max)
+            {
+                return max;
+            }
+            else
+            {
+                return value;
+            }
         }
 
         public static double clamp(double value, double min, double max)
         {
-            if (value < min) {
+            if (value < min)
+            {
                 return min;
             }
-            else if (value > max) {
+            else if (value > max)
+            {
                 return max;
             }
-            else {
+            else
+            {
                 return value;
             }
         }
+
+        
 
         public static double uniform_parabola_k(double x)
         {
